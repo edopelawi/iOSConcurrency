0fb7fc2cc4afb9bca3a882a2ce265079d6cf65bc
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-11-22 13:46:45 +0700

    Update NSOperation Dependencies' AsyncOperation source code to Swift 3

--- ConcurrencyIniOS.playground/Pages/NSOperation Dependencies.xcplaygroundpage/Sources/AsyncOperation.swift
+++ ConcurrencyIniOS.playground/Pages/NSOperation Dependencies.xcplaygroundpage/Sources/AsyncOperation.swift
@@ -23,23 +23,23 @@
 
 import Foundation
 
-public class AsyncOperation: NSOperation {
+public class AsyncOperation: Operation {
   public enum State: String {
-    case Ready, Executing, Finished
+    case ready, executing, finished
     
-    private var keyPath: String {
-      return "is" + rawValue
+    fileprivate var keyPath: String {
+      return "is" + rawValue.capitalized
     }
   }
   
-  public var state = State.Ready {
+  public var state = State.ready {
     willSet {
-      willChangeValueForKey(newValue.keyPath)
-      willChangeValueForKey(state.keyPath)
+      willChangeValue(forKey: newValue.keyPath)
+      willChangeValue(forKey: state.keyPath)
     }
     didSet {
-      didChangeValueForKey(oldValue.keyPath)
-      didChangeValueForKey(state.keyPath)
+      didChangeValue(forKey: oldValue.keyPath)
+      didChangeValue(forKey: state.keyPath)
     }
   }
 }
@@ -47,33 +47,33 @@ public class AsyncOperation: NSOperation {
 
 extension AsyncOperation {
   // NSOperation Overrides
-  override public var ready: Bool {
-    return super.ready && state == .Ready
+  override public var isReady: Bool {
+    return super.isReady && state == .ready
   }
   
-  override public var executing: Bool {
-    return state == .Executing
+  override public var isExecuting: Bool {
+    return state == .executing
   }
   
-  override public var finished: Bool {
-    return state == .Finished
+  override public var isFinished: Bool {
+    return state == .finished
   }
   
-  override public var asynchronous: Bool {
+  override public var isAsynchronous: Bool {
     return true
   }
   
   override public func start() {
-    if cancelled {
-      state = .Finished
+    if isCancelled {
+      state = .finished
       return
     }
     
     main()
-    state = .Executing
+    state = .executing
   }
   
   public override func cancel() {
-    state = .Finished
+    state = .finished
   }
 }
