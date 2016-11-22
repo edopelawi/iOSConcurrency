4e14ba08765b93c0e5a1fdbd68fcd50a83416cb9
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-11-22 13:16:06 +0700

    Update Grand Central Dispatch page's code to Swift 3

--- ConcurrencyIniOS.playground/Pages/Grand Central Dispatch.xcplaygroundpage/Contents.swift
+++ ConcurrencyIniOS.playground/Pages/Grand Central Dispatch.xcplaygroundpage/Contents.swift
@@ -4,41 +4,49 @@
  NSOperation queues are built on top of a technology called `libdispatch`, or *Grand Central Dispatch*. This is an advanced open-source technology that underpins concurrent programming on Apple technologies. It uses the now-familiar queuing model to greatly simplify concurrent programming, but is a C-level interface. This can make it slightly more challenging to work with.
  */
 import UIKit
-import XCPlayground
+import PlaygroundSupport
 
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+PlaygroundPage.current.needsIndefiniteExecution = true
 /*:
  ### Using a Global Queue
  iOS has some global queues, where every task eventually ends up being executed. You can use these directly. You need to use the main queue for UI updates.
  */
-let queue = dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
-let mainQueue = dispatch_get_main_queue()
+let queue =  DispatchQueue.global(qos: .userInitiated)
+let mainQueue = DispatchQueue.main
 
 //: ### Creating your own Queue
 //: Creating your own queues allow you to specify a label, which is super-useful for debugging.
 //: You can specify whether the queue is serieal (default) or concurrent (see later).
 //: You can also specify the QOS or priority (here be dragons)
-let attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INITIATED, 0)
-let workerQueue = dispatch_queue_create("com.raywenderlich.worker", attr)
+
+let workerQueue = DispatchQueue(label: "com.raywenderlich.worker", qos: .userInitiated)
 
 
 //: ### Getting the queue name
 //: You can't get hold of the "current queue", but you can obtain its name - useful for debugging
+
 func currentQueueName() -> String? {
-  let label = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL)
-  return String(CString: label, encoding: NSUTF8StringEncoding)
+	// TODO: This is a horrible workaround, since there's no equivalent to the dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL) on Swift 3. Revisit whether there's a better way to do this.
+  let label = __dispatch_queue_get_label(nil)
+  return String(cString: label, encoding: String.Encoding.utf8)
 }
 
 let currentQueue = currentQueueName()
-print(currentQueue)
+print(currentQueue ?? "failed to retrieve queue")
 
 
 //: ### Dispatching work asynchronously
 //: Send some work off to be done, and then continue on—don't await a result
 print("=== Sending asynchronously to Worker Queue ===")
-dispatch_async(workerQueue) { 
-  print("=== ASYNC:: Executing on \(currentQueueName()) ===")
+
+workerQueue.async {
+	print("=== ASYNC:: Executing on \(currentQueueName()) ===")
 }
+
+workerQueue.async { 
+	
+}
+
 print("=== Completed sending asynchronously to worker queue ===\n")
 
 
@@ -46,10 +54,10 @@ print("=== Completed sending asynchronously to worker queue ===\n")
 //: ### Dispatching work synchronously
 //: Send some work off and wait for it to complete before continuing (here be more dragons)
 print("=== Sending SYNChronously to Worker Queue ===")
-dispatch_sync(workerQueue) { 
-  print("=== SYNC:: Executing on \(currentQueueName()) ===")
-}
 
+workerQueue.sync {
+	print("=== SYNC:: Executing on \(currentQueueName()) ===")
+}
 
 print("=== Completed sending synchronously to worker queue ===\n")
 
@@ -63,24 +71,23 @@ func doComplexWork() {
 }
 
 print("=== Starting Serial ===")
-dispatch_async(workerQueue, doComplexWork)
-dispatch_async(workerQueue, doComplexWork)
-dispatch_async(workerQueue, doComplexWork)
-dispatch_async(workerQueue, doComplexWork)
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
+workerQueue.async(execute: doComplexWork)
 
 sleep(5)
 
-let concurrentQueue = dispatch_queue_create("com.raywenderlich.concurrent", DISPATCH_QUEUE_CONCURRENT)
+let concurrentQueue = DispatchQueue(label: "com.raywenderlich.concurrent", attributes: .concurrent)
 
 print("\n=== Starting concurrent ===")
-dispatch_async(concurrentQueue, doComplexWork)
-dispatch_async(concurrentQueue, doComplexWork)
-dispatch_async(concurrentQueue, doComplexWork)
-dispatch_async(concurrentQueue, doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
+concurrentQueue.async(execute: doComplexWork)
 
 sleep(5)
 
-XCPlaygroundPage.currentPage.finishExecution()
-
+PlaygroundPage.current.finishExecution()
 
 //: [➡ GCD Groups](@next)
